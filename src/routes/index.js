641f0d07b402d1e9c6a11017ddc919764810bf91
Damar Adi N <damaradii.nugroho@gmail.com>
2024-11-28 23:24:25 +0700

    create category feature

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const routes = express.Router();
 const testRoutes = require("./testRoutes");
+const categoryRoutes = require("./categoryRoutes");
 
 // List of routes
 routes.use(testRoutes);
+routes.use(categoryRoutes);
 
 module.exports = routes;
