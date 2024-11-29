02201f2c6477b3a3b00322002e4b408d79a4fc79
Damar Adi N <damaradii.nugroho@gmail.com>
2024-11-29 12:38:00 +0700

    update user

--- src/models/user.js
+++ src/models/user.js
@@ -22,11 +22,12 @@ const userSchema = new mongoose.Schema(
     phoneNumber: {
       type: String,
       default: "",
+      minlength: 11,
     },
     password: {
       type: String,
-      required: [true, "Password Wajib Diisi"],
-      minlength: [6, "Password Wajib Minimal 6 Karakter"],
+      required: [true, "password is required."],
+      minlength: [6, "password must be at least 6 characters long."],
     },
     nationalIdentityCard: {
       type: String,
@@ -56,15 +57,12 @@ const phoneRegex = /^(?:\+62|62|0)8[1-9]\d{8,9}$/;
 
 userSchema.pre("save", async function (next) {
   if (this.phoneNumber) {
-    // Menghapus semua spasi
     this.phoneNumber = this.phoneNumber.replace(/\s+/g, "");
 
-    // Mengganti awalan '0' dengan '+62' jika ada
     if (this.phoneNumber.startsWith("0")) {
       this.phoneNumber = "+62" + this.phoneNumber.substring(1);
     }
 
-    // Validasi nomor telepon menggunakan regex
     if (!phoneRegex.test(this.phoneNumber)) {
       const error = new Error("Nomor telepon tidak valid");
       return next(error);
