5aa6fc5d265ec9721a0d9e8d9361bf897c3a3c43
azizah89 <azizahpauziah716@gmail.com>
2024-12-13 09:52:12 +0700

    Membuat integrasi profile dan edit profile

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const userRoutes = express.Router();
-const { auth } = require("../middleware");
-const { userController } = require("../controller");
-const { upload } = require("../middleware/upload");
+const {auth} = require("../middleware");
+const {userController} = require("../controller");
+const {upload} = require("../middleware/upload");
 
 userRoutes.post("/user/login", userController.login);
 userRoutes.post("/user/register", userController.register);
@@ -13,7 +13,11 @@ userRoutes.post(
   userController.Upload
 );
 userRoutes.get("/user/profile", auth, userController.getProfile);
-userRoutes.put("/user/profile", auth, userController.updateProfile);
+userRoutes.put("/user/profile"
+  , auth,
+  upload.single("profilePhoto"),
+  userController.updateProfile,
+);
 userRoutes.get("/user/verify/:uniqueString", userController.verifyEmail);
 userRoutes.get("/user/verifed", auth, userController.verified);
 userRoutes.post(
