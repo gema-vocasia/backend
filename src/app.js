650481e891456cf198ff255d9b3aadf920b19798
Damar Adi N <damaradii.nugroho@gmail.com>
2024-12-14 19:42:50 +0700

    updating

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 // Koneksi ke database
 connectDB();
